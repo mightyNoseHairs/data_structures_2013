4369c93ac5541093faa510197f23198743b6c9d3
mightyNoseHairs <wagnercarl46@gmail.com>
2022-02-25 03:38:49 +0000

    gcd

--- main.py
+++ main.py
@@ -2,6 +2,19 @@
 '''
 def main():
     print("Hello World!")
+    def gcd (m, n):
+        if m<1 or n<1:
+            return 0
+        p, q = m, n
+        if m<n:
+            p, q = n, m
+        while ((p%q) != 0):
+            r = p % q
+            p, q = q, r
+        return q
+    print(gcd(77, 21)) #7
+
+
 
 if __name__ == "__main__":
     main()
